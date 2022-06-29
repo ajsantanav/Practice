60e4483bf4262836fb6fefb13b0ed9b34142bd7c
Adrian Santana <adriansantana.v@gmail.com>
2022-06-29 11:52:38 -0400

    2022/6/29 Practice Add

---
+++ src/Addition.java
@@ -0,0 +1,19 @@
+public class Addition {
+    public static void main(String[] args) {
+        int val1 = 12, val2 = 5, val3;
+
+
+        val3 = sum(12,5);
+
+        System.out.println(val3);
+
+    }
+
+    public static int sum(int num1, int num2) {
+
+//        int sum;
+//        sum = num1 + num2;
+
+        return num1 + num2;
+    }
+}
